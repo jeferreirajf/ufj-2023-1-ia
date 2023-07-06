602b107932bbba1bb14dd247105a60ecc5c04c6c
jeferreira <jeferreirajf@gmail.com>
2023-07-06 09:46:27 -0300

    Add comments knapsack-problem index.ts

--- aula04/examples/knapsack-problem/src/index.ts
+++ aula04/examples/knapsack-problem/src/index.ts
@@ -1,12 +1,17 @@
 import { ProblemFactory } from "./problem-factory";
 import { KnapSackSolver } from "./solver";
 
+// Construção do cenário do problema
 const aProblem = ProblemFactory.BuildProblem1();
 
+// Imprime dados na tela
 console.log(aProblem);
 
+// Construção de um "resolvedor"
 const aSolver = new KnapSackSolver(aProblem);
 
+// Chamada da construção da solução para o problema
 const aSolution = aSolver.solve();
 
+// Impressão da solução na tela
 console.log("Solution: ", aSolution);
